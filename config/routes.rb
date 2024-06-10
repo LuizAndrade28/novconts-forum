e913955cec3f64fd165d767b6cb58600f6cb1431
LuizAndrade <lcfsandrade@gmail.com>
2024-06-10 15:29:49 -0300

    Nav avatar devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'entrar',
+    sign_out: 'sair',
+    password: 'senha',
+    registration: '',
+    sign_up: 'cadastrar',
+    edit: 'editar',
+    cancel: 'cancelar'
+  }
+
   root to: "pages#home"
 
-  resources :posts do
+  resources :posts, except: %i[index] do
     resources :comments, only: %i[new create destroy]
   end
+
+  get "meus_posts", to: "posts#index", as: :meus_posts
 end
