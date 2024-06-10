c025ee4fdabf91cae1bd2ac2b95a2eccad2c98fc
LuizAndrade <lcfsandrade@gmail.com>
2024-06-09 22:56:11 -0300

    Initial layout sizing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :content, presence: true
+  validates :content, length: { maximum: 1000 }
+
+  default_scope -> { order(created_at: :desc) }
+  # paginates_per 10
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :title, length: { maximum: 255 }
+  validates :content, length: { maximum: 1000 }
+
+  default_scope -> { order(created_at: :desc) }
+  # paginates_per 10
+
 end
