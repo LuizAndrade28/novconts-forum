8e16e84f5ffb5b95164acc1c30f768e1841951e5
LuizAndrade <lcfsandrade@gmail.com>
2024-06-11 00:48:37 -0300

    Home, devise, forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,16 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
+  validates :title, uniqueness: true
   validates :content, presence: true
   validates :title, length: { maximum: 255 }
   validates :content, length: { maximum: 1000 }
 
-  default_scope -> { order(created_at: :desc) }
-  # paginates_per 10
+  def self.ransackable_associations(_auth_object = nil)
+    ["user"]
+  end
 
+  def self.ransackable_attributes(_auth_object = nil)
+    ["title"]
+  end
 end
